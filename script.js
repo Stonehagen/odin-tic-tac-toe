a86f6ec1d41ef6a2ebe6be92aae8f15195383f4c
Stonehagen <steinhagen@mailbox.org>
2022-12-31 23:43:40 +0100

    implement check winner

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 /* eslint-disable func-names */
 /* eslint-disable wrap-iife */
 const Gameboard = (function () {
-  const gameboard = Array(9).fill(' ');
+  let gameboard = Array(9).fill(' ');
+  const winPattern = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  let winner = ' ';
 
   const createPlayer = (sign, name = 'player') => ({ sign, name });
 
@@ -10,13 +21,45 @@ const Gameboard = (function () {
 
   let xTurn = true;
 
+  const getWinner = () => winner;
+
+  const checkWinner = () => {
+    winPattern.forEach((pattern) => {
+      if (gameboard[pattern[0]] !== ' ') {
+        if (
+          gameboard[pattern[0]] === gameboard[pattern[1]] &&
+          gameboard[pattern[0]] === gameboard[pattern[2]]
+        ) {
+          winner = gameboard[pattern[0]];
+          return true;
+        }
+      }
+    });
+    return false;
+  };
+
+  const getWinningText = () => {
+    checkWinner();
+    if (winner !== ' ') {
+      return `Winner: ${winner}`;
+    }
+    return ' ';
+  };
+
   const getGameboard = () => gameboard;
 
   const setGameboard = (index) => {
     if (gameboard[index] !== ' ') return;
     gameboard[index] = xTurn ? playerX.sign : playerO.sign;
-    DisplayController.render(gameboard);
     xTurn = !xTurn;
+    checkWinner();
+    DisplayController.render(gameboard);
+  };
+
+  const restart = () => {
+    gameboard = Array(9).fill(' ');
+    winner = ' ';
+    xTurn = true;
   };
 
   const createCell = (value, index) => {
@@ -27,7 +70,7 @@ const Gameboard = (function () {
     return cell;
   };
 
-  const addClickEvent = (cells, sign = 'T') => {
+  const addClickEvent = (cells) => {
     cells.forEach((cell) => {
       cell.addEventListener('click', () => {
         setGameboard(cell.id);
@@ -40,6 +83,9 @@ const Gameboard = (function () {
     setGameboard,
     createCell,
     addClickEvent,
+    getWinner,
+    getWinningText,
+    restart,
   };
 })();
 
@@ -51,6 +97,10 @@ const DisplayController = (function () {
       document.querySelector('.gameboard').appendChild(cell);
     });
     Gameboard.addClickEvent(document.querySelectorAll('.cell'));
+    document.querySelector('.info').innerHTML = Gameboard.getWinningText();
+    if (Gameboard.getWinner() !== ' ') {
+      Gameboard.restart();
+    }
   };
 
   return { render };
